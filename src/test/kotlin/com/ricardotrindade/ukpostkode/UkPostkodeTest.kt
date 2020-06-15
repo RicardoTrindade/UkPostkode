e97f5937e666070a9f77da67c6b2d3b8270a28cd
Ricardo Trindade <ricardo.trindade743@gmail.com>
2020-06-15 16:32:57 +0100

    more tests

--- src/test/kotlin/com/ricardotrindade/ukpostkode/UkPostkodeTest.kt
+++ src/test/kotlin/com/ricardotrindade/ukpostkode/UkPostkodeTest.kt
@@ -4,7 +4,27 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class UkPostkodeTest {
-    @Test fun testBase() {
-        assertEquals("N1 5QJ", UkPostkode("N1 5QJ").full())
+    @Test fun testN15QJ() {
+        val postcode = UkPostkode("N1 5QJ")
+        assertEquals("N1 5QJ", postcode.full())
+
+        assertEquals("5QJ", postcode.incode)
+        assertEquals("N1", postcode.outcode)
+        assertEquals("N", postcode.area)
+        assertEquals("1", postcode.district)
+        assertEquals("5", postcode.sector)
+        assertEquals("QJ", postcode.unit)
+    }
+
+    @Test fun testE84AA() {
+        val postcode = UkPostkode("E8 4AA")
+        assertEquals("E8 4AA", postcode.full())
+
+        assertEquals("4AA", postcode.incode)
+        assertEquals("E8", postcode.outcode)
+        assertEquals("E", postcode.area)
+        assertEquals("8", postcode.district)
+        assertEquals("4", postcode.sector)
+        assertEquals("AA", postcode.unit)
     }
 }
